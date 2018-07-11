e7918f59be437c20c286ae0631e2b2dc9a81e947
cedi <cedi@users.noreply.github.com>
2018-07-12 00:07:04 +0200

    Fix

--- mininet/deploy.sh
+++ mininet/deploy.sh
@@ -1,10 +1,11 @@
-#!/bin/bash -x
+#!/bin/bash -xe
+
 cd $GOPATH/src/github.com/bio-routing/bio-rd/
 bazel build //:bio-rd
 
 scp $GOPATH/src/github.com/bio-routing/bio-rd/bazel-bin/linux_amd64_stripped/bio-rd vagrant:/usr/local/bin/
 scp $HOME/src/bio-rd/LAB-Tools/mininet/* vagrant:~/src/bio-rd/LAB-Tools/mininet/
 
-ssh vagrant "killall tail"
-ssh vagrant "sudo killall lab.py"
+ssh vagrant "killall tail || true"
+ssh vagrant "sudo killall lab.py || true"
 ssh vagrant "tmux -S /tmp/tmux.soccket send-keys -t '0:1.3' C-z 'sudo ./lab.py' Enter"
